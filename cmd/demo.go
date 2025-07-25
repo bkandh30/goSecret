d49a7563a682b14644e20636f9132f63743e0c0f
bkandh30 <kandhari.bhavya20@gmail.com>
2025-07-24 23:37:51 -0700

    added filereader support

--- cmd/demo.go
+++ cmd/demo.go
@@ -7,16 +7,37 @@ import (
 )
 
 func main() {
-	v := secret.Memory("my-key")
-	err := v.Set("demo_key", "some hash value")
+	v := secret.File("my-key", ".secrets")
+	err := v.Set("demo_key1", "123 value")
 	if err != nil {
 		panic(err)
 	}
 
-	plain, err := v.Get("demo_key")
+	err = v.Set("demo_key2", "456 value")
 	if err != nil {
 		panic(err)
 	}
 
+	err = v.Set("demo_key3", "789 value")
+	if err != nil {
+		panic(err)
+	}
+
+	plain, err := v.Get("demo_key1")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Plain:", plain)
+
+	plain, err = v.Get("demo_key2")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Plain:", plain)
+
+	plain, err = v.Get("demo_key3")
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("Plain:", plain)
 }
