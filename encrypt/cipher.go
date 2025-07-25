39abd2cb1a5fc4ef5737fc969a5ca67d88d8f6a7
bkandh30 <kandhari.bhavya20@gmail.com>
2025-07-24 22:29:41 -0700

    implemented cipher logic

---
+++ encrypt/cipher.go
@@ -0,0 +1,66 @@
+package encrypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"io"
+)
+
+func Encrypt(key, plaintext string) (string, error) {
+	block, err := newCipherBlock(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, []byte(plaintext), nil)
+	return hex.EncodeToString(ciphertext), nil
+}
+
+func Decrypt(key, cipherHex string) (string, error) {
+	block, err := newCipherBlock(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext, err := hex.DecodeString(cipherHex)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return "", errors.New("encrypt: ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plaintext), nil
+}
+
+func newCipherBlock(key string) (cipher.Block, error) {
+	hash := sha256.Sum256([]byte(key))
+	return aes.NewCipher(hash[:])
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bkandh30/goSecret
+
+go 1.24.0
